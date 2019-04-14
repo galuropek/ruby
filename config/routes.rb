3bc970d085a9b0e74faa8179314b290fe7ab23da
galuropek <galuropek@gmail.com>
2019-04-14 19:40:21 +0300

    Added gem devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
 
+  devise_for :users
   # root для главной страницы root
-  root 'posts#index', as: 'home'
+  root 'home#index', as: 'home'
 
   # для остальных страниц get
   get 'about' => 'pages#about', as: 'about'
