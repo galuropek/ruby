3ee4a3d6170332b5d2dabcbb3e72294261cca702
galuropek <galuropek@gmail.com>
2019-04-20 22:53:13 +0300

    Added all posts viewer (+ post edit button realisation)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_15_181120) do
+ActiveRecord::Schema.define(version: 2019_04_20_193423) do
 
   create_table "comments", force: :cascade do |t|
     t.string "username"
@@ -41,6 +41,10 @@ ActiveRecord::Schema.define(version: 2019_04_15_181120) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.boolean "admin", default: false
+    t.string "name"
+    t.string "surname"
+    t.string "nickname"
+    t.string "avatar"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
