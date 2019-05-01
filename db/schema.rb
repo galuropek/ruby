7f35afe0b0218417531e93f6d6d120bb23ee82fd
galuropek <galuropek@gmail.com>
2019-05-01 22:20:55 +0300

    Migration for sqlite3.

--- db/schema.rb
+++ db/schema.rb
@@ -24,11 +24,11 @@ ActiveRecord::Schema.define(version: 2019_04_21_093425) do
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "body"
-    t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "time"
     t.string "end_time"
+    t.integer "user_id"
     t.string "image"
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
